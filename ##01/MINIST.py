21082652b8628c4448da1ee78bc18f48ee044631
abakane1 <mrzzl1987@gmail.com>
2018-02-20 13:24:52 -0500

    ##01 minist examples

--- ##01/MINIST.py
+++ ##01/MINIST.py
@@ -71,3 +71,59 @@ cls_true = data.test.cls[0:9]
 
 # Plot the images and labels using our helper-function above.
 plot_images(images=images, cls_true=cls_true)
+
+# the linear computing graph
+x = tf.placeholder(tf.float32, [None, img_size_flat])
+y_true = tf.placeholder(tf.float32,[None, num_classes])
+y_true_cls = tf.placeholder(tf.int64, [None])
+
+weights = tf.Variable(tf.zeros([img_size_flat, num_classes]))
+biases = tf.Variable(tf.zeros([num_classes]))
+
+logits = tf.matmul(x, weights) + biases
+y_pred = tf.nn.softmax(logits)
+y_pred_cls = tf.argmax(y_pred, dimension=1)
+
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_true)
+
+cost = tf.reduce_mean(cross_entropy)
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.5).minimize(cost)
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+
+
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    batch_size = 100
+    feed_dict_test = {x: data.test.images,
+                      y_true: data.test.labels,
+                      y_true_cls: data.test.cls}
+
+    num_iterations = 1000
+    for i in range(num_iterations):
+        # Get a batch of training examples.
+        # x_batch now holds a batch of images and
+        # y_true_batch are the true labels for those images.
+        x_batch, y_true_batch = data.train.next_batch(batch_size)
+
+        # Put the batch into a dict with the proper names
+        # for placeholder variables in the TensorFlow graph.
+        # Note that the placeholder for y_true_cls is not set
+        # because it is not used during training.
+        feed_dict_train = {x: x_batch,
+
+                           y_true: y_true_batch}
+
+        # Run the optimizer using this batch of training data.
+        # TensorFlow assigns the variables in feed_dict_train
+        # to the placeholder variables and then runs the optimizer.
+        sess.run(optimizer, feed_dict=feed_dict_train)
+        # todo
+        acc = sess.run(accuracy,feed_dict=feed_dict_test)
+        print(acc)
+
