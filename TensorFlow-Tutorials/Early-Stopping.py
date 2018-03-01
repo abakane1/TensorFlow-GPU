1dd321b6e23955640dec51cc826d66ffccf0a84e
abakane1 <mrzzl1987@gmail.com>
2018-02-28 22:54:51 -0500

    delete useless imports

--- TensorFlow-Tutorials/Early-Stopping.py
+++ TensorFlow-Tutorials/Early-Stopping.py
@@ -1,14 +1,8 @@
 # matplotlib inline
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import numpy as np
-from sklearn.metrics import confusion_matrix
-import time
-from datetime import timedelta
-import math
-from tensorflow.examples.tutorials.mnist import input_data
 import os
+
 import CNN
+import tensorflow as tf
 
 saver = tf.train.Saver()
 
@@ -17,7 +11,8 @@ saver_dir = 'checkpoints'
 if not os.path.exists(saver_dir):
     os.makedirs(saver_dir)
 
-save_path = os.path.join(saver_dir,'best_validation')
+save_path = os.path.join(saver_dir, 'best_validation')
+
 
 def Init_store():
     CNN.init_variables()
@@ -25,11 +20,7 @@ def Init_store():
     CNN.optimize(2000)
     saver.save(sess=CNN.session, save_path=save_path)
 
-#Init_store()
+
+# Init_store()
 saver.restore(sess=CNN.session, save_path=save_path)
 CNN.print_test_accuracy()
-
-
-
-
-
