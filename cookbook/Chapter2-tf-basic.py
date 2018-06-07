71442c7ff75eb1b9a32418be3811306d57f76a31
abakane1 <mrzzl1987@gmail.com>
2018-06-07 16:26:13 -0400

    first rnn sample

--- cookbook/Chapter2-tf-basic.py
+++ cookbook/Chapter2-tf-basic.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 a = tf.constant(1)
 b = tf.constant(2)
@@ -21,4 +22,16 @@ def ArrayTF():
         outs = sess.run(fetches)
     print(outs)
 
-ArrayTF()
+
+#ArrayTF()
+
+def ArraysandShapes():
+    c = tf.constant(np.array([
+        [[1,2,3],
+        [4,5,6]],
+        [[1,1,1],
+         [2,2,2]]
+    ]))
+    print(format(c.get_shape()))
+
+ArraysandShapes()
